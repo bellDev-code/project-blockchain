e8e9a5c84ee3497c45aece1407a5ef5f982b5fb4
bellDev-code <sumaoo20@naver.com>
2021-06-22 16:32:58 +0900

    js form hidden and innerText add

--- nomad/app.js
+++ nomad/app.js
@@ -1,21 +1,18 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-// const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
   event.preventDefault();
-  console.log(loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  // greeting.innerText = "Hello " + username;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-// function onLoginButtonClick() {
-// const username = loginButton.value
-// username 유효성 검사!!
-// javascript만을 사용한다면 필수적인 과정이나 현재 login form을 활용할 예정
-// html 안에서 유효성 검사하는 법이 있다.
-// if (username === "") {
-//   console.log("Please write your name");
-// } else if (username.length > 15) {
-//   alert("your name is too long.");
-//}
-// loginButton.addEventListener("click", onLoginButtonClick);
+//addEventListener는 함수를 실행하지 않는다.
+// 브라우저가 실행하게 도와주는 것.
 loginForm.addEventListener("submit", onLoginSubmit);
