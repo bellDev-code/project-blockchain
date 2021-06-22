90fb59031dff0340f1acd182bc39f2b0c2875e8f
bellDev-code <sumaoo20@naver.com>
2021-06-22 10:31:23 +0900

    hmtl Responsive Web and javascript event part two

--- nomad/app.js
+++ nomad/app.js
@@ -1,7 +1,42 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
 function handleTitleClick() {
-  title.style.color = "blue";
+  h1.style.color = "blue";
 }
 
-title.addEventListener("click", handleTitleClick);
+function handleMouseEnter() {
+  h1.innerText = "Mouse is here!!";
+}
+
+function handleMouseLeave() {
+  h1.innerText = "Mouse is gone!!";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOD");
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+// 이렇게도 표현 가능 가독성이 떨어짐.
+// h1.onclick = handleTitleClick;
+// h1.onmouseenter = handleMouseEnder;
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
