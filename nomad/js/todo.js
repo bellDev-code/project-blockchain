18ba7b6c2e61d4810a6f8d6f01082f0a6d105876
bellDev-code <sumaoo20@naver.com>
2021-06-28 16:34:46 +0900

    todo localStorage add

--- nomad/js/todo.js
+++ nomad/js/todo.js
@@ -2,13 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const toDos = [];
+
+// toDos를 단지 localStorage에다가 넣어주는 역할
+function saveToDos() {
+  // JSON.stringify() 문자열로 만들어준다.
+  localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
 // parentElement는 클릭된 element의 부모다.
 function deleteTodo(event) {
   const li = event.target.parentElement;
   li.remove();
 }
 
-// 새로고침 누르면 지워지는 단점.
+// 새로고침 누르면 지워지는 단점. => 해결책 : locarStorage
 // todolist add function
 function paintToDo(newTodo) {
   // console.log("I will paint", newTodo);
@@ -28,7 +36,10 @@ function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
+  // toDos = []에다가 push(newTodo) 한다.
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 // toDoForm 이벤트를 추가한 부분
