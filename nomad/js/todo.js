a40045cbb2ecbf7f386e9636c15fd39c01cb6b4d
bellDev-code <sumaoo20@naver.com>
2021-06-29 17:00:52 +0900

    loading toDos localStorage JSON.stringfy and JSON.parse

--- nomad/js/todo.js
+++ nomad/js/todo.js
@@ -2,12 +2,16 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
-const toDos = [];
+const TODOS_KEY = "todos";
+
+// const toDos = []; // 시작할 때 항상 비어져있다.
+// localStorate에서 발견되는 이전의 toDo들로 하고 싶음
+let toDos = [];
 
 // toDos를 단지 localStorage에다가 넣어주는 역할
 function saveToDos() {
   // JSON.stringify() 문자열로 만들어준다.
-  localStorage.setItem("todos", JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 // parentElement는 클릭된 element의 부모다.
@@ -32,6 +36,7 @@ function paintToDo(newTodo) {
 }
 
 // todolist value 값 submit 되면 toDoInput에 적힌 value 값을 가진다.
+// submit 되면, saveToDos 함수 호출한다.
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
@@ -44,3 +49,15 @@ function handleToDoSubmit(event) {
 
 // toDoForm 이벤트를 추가한 부분
 toDoForm.addEventListener("submit", handleToDoSubmit);
+
+// savedToDos는 localStorage에서 가져오는 "["1", "2", "3"]" string 형태이다.
+const savedToDos = localStorage.getItem(TODOS_KEY);
+// console.log(savedToDos);
+if (savedToDos) {
+  // string 형태를 JSON.parse를 사용하여 자바스크립트에서 쓸 수 있는 array로 만든다.
+  const parsedToDos = JSON.parse(savedToDos);
+  // console.log(parsedToDos); // ["1", "2", "3"]
+  toDos = parsedToDos;
+  // 각각의 item에다가 console.log()한다.
+  parsedToDos.forEach(paintToDo);
+}
